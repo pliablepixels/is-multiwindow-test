da21f08a9c2cb3c4cf2872ecd55420901df271f5
Pliable Pixels <pliablepixels@gmail.com>
2018-08-19 06:38:15 -0400

    added timeout for async scope

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,14 +1,14 @@
 angular.module('starter', ['ionic'])
-.controller('StarterCtrl', function($scope) {
-        $scope.state = "";
+.controller('StarterCtrl', function($scope, $timeout) {
+        $scope.state = "unknown";
 
 $scope.isMultiWindow = function() {
      window.IsMultiWindowPlugin.get(function (state) {
-             $scope.state = state;
+             $timeout (function() {$scope.state = state});
     },
     function (err) {
         console.log (" *************** ERR:"+JSON.stringify(err));
-        $scope.state = "Error";
+        $scope.state = "error:"+JSON.stringify(err);
     });
 }
 
