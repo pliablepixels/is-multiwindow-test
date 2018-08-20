c41d73dcdf01b919375a2c7571232e55e3a8dcc3
Pliable Pixels <pliablepixels@gmail.com>
2018-08-20 10:50:11 -0400

    0.0.2 plugin related updates

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,19 +1,32 @@
 angular.module('starter', ['ionic'])
-.controller('StarterCtrl', function($scope, $timeout) {
+.controller('StarterCtrl', function($scope, $timeout, $ionicPlatform) {
         $scope.state = "unknown";
+        var startCount = 0;
+        var stopCount = 0;
 
-$scope.isMultiWindow = function() {
-     window.MultiWindowPlugin.get(function (state) {
-             $timeout (function() {$scope.state = state});
-    },
-    function (err) {
-        console.log (" *************** ERR:"+JSON.stringify(err));
-        $scope.state = "error:"+JSON.stringify(err);
-    });
-}
+        $ionicPlatform.ready (function () {
+            window.MultiWindowPlugin.registerOnStop(onStop);
+            window.MultiWindowPlugin.registerOnStart(onStart);
+        });
 
+        $scope.isMultiWindow = function() {
+             window.MultiWindowPlugin.get(function (data) {
+                  $timeout (function() {$scope.state = data.state});
+            },
+            function (err) {
+                console.log (" *************** ERR:"+JSON.stringify(err));
+                $scope.state = "error:"+JSON.stringify(err);
+            });
+        };
 
+        function onStart(data) {
+                startCount++;
+                console.log ("JS LAND - got onStart "+startCount+" times" );
+        }
 
-
+        function onStop(data) {
+                stopCount++;
+                console.log ("JS LAND - got onStop " + stopCount+" times");
+        }
 })
 
