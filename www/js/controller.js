7c165767999a245e522a26dbd9ba33987c5f9a17
Pliable Pixels <pliablepixels@gmail.com>
2018-08-20 15:25:54 -0400

    edits

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,29 +1,21 @@
 angular.module('starter', ['ionic'])
-.controller('StarterCtrl', function($scope, $timeout, $ionicPlatform) {
+.controller('StarterCtrl', function($scope, $timeout, $ionicPlatform, $ionicPopup) {
         $scope.state = "unknown";
         var startCount = 0;
         var stopCount = 0;
 
         $ionicPlatform.ready (function () {
+            console.log ("Registering callbacks...");
             window.MultiWindowPlugin.registerOnStop(onStop);
             window.MultiWindowPlugin.registerOnStart(onStart);
         });
-        $scope.isMultiWindow = function() {
-          window.MultiWindowPlugin.get(function (state) {
-             $timeout (function() {$scope.state = state});
-          },
-          function (err) {
-             console.log (" *************** ERR:"+JSON.stringify(err));
-             $timeout (function() {$scope.state = "error:"+JSON.stringify(err);});
-          });
-        }
 
         $scope.isMultiWindow = function() {
              window.MultiWindowPlugin.get(function (data) {
                   $timeout (function() {$scope.state = data.state});
             },
             function (err) {
-                console.log (" *************** ERR:"+JSON.stringify(err));
+                console.log ("ERR:"+JSON.stringify(err));
                 $scope.state = "error:"+JSON.stringify(err);
             });
         };
